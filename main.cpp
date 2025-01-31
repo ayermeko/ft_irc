322379ef72adb092e923333e337f4c8010e22a67
Alibi Yermekov <ayermeko@c1r1s4.42prague.com>
2025-01-31 19:59:36 +0100

    modified files

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 int main(int argc, char const *argv[])
 {
     if (argc > 3)
-    {
         throw (std::runtime_error("Guide: ./ircserv <port> <password>"));
-    }
     Server server(argv[1], argv[2]);
     try
     {
+        server.start();
         return (0);
     }
     catch(const std::exception& e)
