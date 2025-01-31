223c40f74e5bbc8683e7c3d94f610cc8a731c07d
Alibi <yoooholly71@gmail.com>
2025-01-31 16:18:50 +0100

    pp

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
+#include <sys/socket.h>
+#include <sys/poll.h>
+
+#include <string>
+#include <vector>
+
 class Client
 {
-private:
-    /* data */
-public:
-    Client(/* args */);
-    ~Client();
+    private:
+        int _fd;
+        int _port;
+        Client();
+        Client(const Client &src);
+    public:
+        Client(int fd, int port, const std::string &hostname);
+        ~Client();
 };
-
-Client::Client(/* args */)
-{
-}
-
-Client::~Client()
-{
-}
---
+++ srcs/Client.cpp
@@ -0,0 +1,8 @@
+#include "includes/Client.hpp"
+
+Client::Client(int fd, int port, const std::string &hostname)
+{
+    
+}
+
+Client::~Client() {}
